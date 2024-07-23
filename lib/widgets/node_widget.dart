6a704d715184a540bd4526b40338c6fef477aced
Mohammadb <mohammad.mb.bashirinia@gmail.com>
2024-07-23 11:56:12 +0330

    edit node widget

--- lib/widgets/node_widget.dart
+++ lib/widgets/node_widget.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mind_map/models/node_model.dart';
 
-class NodeWidget extends StatelessWidget {
+class NodeWidget extends StatefulWidget {
   final NodeModel nodeModel;
   final void Function(DragUpdateDetails details)? onPanUpdate;
   final void Function()? onDoubleTap;
   final void Function()? onLongPress;
+
   const NodeWidget({
     super.key,
     required this.nodeModel,
@@ -15,17 +17,33 @@ class NodeWidget extends StatelessWidget {
   });
 
   @override
+  State<NodeWidget> createState() => _NodeWidgetState();
+}
+
+class _NodeWidgetState extends State<NodeWidget> {
+  bool isHoverd = false;
+  @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: nodeModel.pos.dx,
-      top: nodeModel.pos.dy,
-      child: MouseRegion(
-        cursor: SystemMouseCursors.grab,
-        child: GestureDetector(
-            onDoubleTap: onDoubleTap,
-            onPanUpdate: onPanUpdate,
-            onLongPress: onLongPress,
-            child: Container(
+      left: widget.nodeModel.pos.dx,
+      top: widget.nodeModel.pos.dy,
+      child: GestureDetector(
+        onDoubleTap: widget.onDoubleTap,
+        onPanUpdate: widget.onPanUpdate,
+        onLongPress: widget.onLongPress,
+        child: MouseRegion(
+          onEnter: (event) {
+            print(isHoverd);
+            isHoverd = true;
+            setState(() {});
+          },
+          onExit: (event) {
+            print(isHoverd);
+            isHoverd = false;
+            setState(() {});
+          },
+          cursor: isHoverd ? SystemMouseCursors.grab : MouseCursor.defer,
+          child: Container(
               width: 160,
               height: 90,
               padding: const EdgeInsets.all(4),
@@ -39,7 +57,7 @@ class NodeWidget extends StatelessWidget {
                   )
                 ],
                 color: Colors.white,
-                border: Border.all(width: 2, color: nodeModel.color),
+                border: Border.all(width: 2, color: widget.nodeModel.color),
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Column(
@@ -47,21 +65,16 @@ class NodeWidget extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  nodeModel.imageUrl!.isNotEmpty
-                      ? Image.network(
-                          nodeModel.imageUrl ?? "",
+                  widget.nodeModel.imageUrl!.isNotEmpty
+                      ? Image.file(
+                          File(widget.nodeModel.imageUrl!),
                           fit: BoxFit.cover,
                           width: 60,
                           height: 40,
-                          // errorBuilder: (context, error, stackTrace) {
-                          //   return Icon(
-                          //     CupertinoIcons.clear_circled,color: Colors.red,
-                          //   );
-                          // },
                         )
                       : Container(),
                   Text(
-                    nodeModel.name ?? "",
+                    widget.nodeModel.name ?? "",
                     style: const TextStyle(fontSize: 12),
                   ),
                   Expanded(
@@ -71,12 +84,13 @@ class NodeWidget extends StatelessWidget {
                           style: const TextStyle(fontSize: 12),
                           overflow: TextOverflow.ellipsis,
                           maxLines: 1,
-                          nodeModel.des ?? ""),
+                          widget.nodeModel.des ?? ""),
                     ),
                   ),
                 ],
               ),
-            )),
+            ),
+        ),
       ),
     );
   }
